0729d2ae9d8b3a72fa6d1cadf144d730d66509c3
deft <deft1991@gmail.com>
2020-09-24 19:54:18 +0300

    Initial commit

---
+++ src/test/java/com/deft/petproject/PetprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deft.petproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
